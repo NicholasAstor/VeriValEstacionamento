3a96b84865ca1c67ef65f7290676146e17e38696
NICHOLAS NAVARRO <nicholas.barbosa@edu.pucrs.br>
2024-10-08 20:02:20 -0300

    Adicionei mais alguns casos de teste e alguns comentários na classe Test

--- src/test/java/CalculadoraEstacionamentoTest.java
+++ src/test/java/CalculadoraEstacionamentoTest.java
@@ -32,8 +32,18 @@ public class CalculadoraEstacionamentoTest {
         // Comum sem pernoite
         "'2024-10-10T08:00', '2024-10-11T07:00', false, 63.40",
         // VIP sem pernoite
-        "'2024-10-10T08:00', '2024-10-11T07:00', true, 31.70"
+        "'2024-10-10T08:00', '2024-10-11T07:00', true, 31.70",
+        // Sair no horário em que o estacionamento esta fechado
+        "'2024-10-10T08:00', '2024-10-11T04:00', true, 0.0",
+        //Formato de data colocado ao contrário
+        "'10-10-2024T08:00', '10-10-2024T08:15', true, 0.0"
     })
+
+    /*Esses cobrem todos os testes que deveriam ser feitos para garantir o bom funcionamento desse programa,
+     * os testes que deram errado foram os 3 últimos, sendo o ("Comum sem pernoite", valorEsperado=63.4, valorRecebido=60,9),
+     * ("VIP sem pernoite", valorEsperado = 31.70, valorRecebido = 30.45) e por último um teste que eu não sei
+     * se necessita estar aqui mas ("Formato de data colocado ao contrário", valorEsperado=0.0, da erro).
+     */
     public void testCalculoEstacionamento(String entradaStr, String saidaStr, boolean isVip, double valorEsperado) {
         LocalDateTime entrada = LocalDateTime.parse(entradaStr);
         LocalDateTime saida = LocalDateTime.parse(saidaStr);
@@ -42,5 +52,4 @@ public class CalculadoraEstacionamentoTest {
 
         assertEquals(valorEsperado, valorCalculado);
     }
-    //Me da erro nos dois últimos testes mas talvez eu tenha errado no cálculo e a implementação esteja certa
 }
